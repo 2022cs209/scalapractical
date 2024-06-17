719cbbfc439844cb2c3e4341922597f830a39326
Pabodya Nethsarani <2022cs209@stu.ucsc.cmb.ac.lk>
2024-06-17 11:15:25 +0530

    Add files via upload

---
+++ new1.scala
@@ -0,0 +1,6 @@
+def area_of_disk(radius: Double):Double =  Math.PI * radius * radius
+def temperature_Farenheit(celcius : Double): Double = celcius * 1.8000 + 32.00
+def volume_of_sphere (radius: Double) : Double = 4/3 * Math.PI * radius * radius * radius
+def total_cost () : Double = (24.95 * 60 ) - ( 50 * 3) - (0.75 * 10) + (24.95 * 0.4 * 60)
+def total_run_time() : Double = ( 8 * 2) + ( 3 * 7) + ( 8 * 2)
+ 
